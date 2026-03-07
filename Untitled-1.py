9128160959388a3f3514c8a1a3191d6ddfc3eb06
Hamidur Rahman Majed <2022334042@student.sust.edu>
2026-03-08 01:16:25 +0600

    title case manually

---
+++ Untitled-1.py
@@ -0,0 +1,17 @@
+def title_man(name):
+    n = name.split()
+    rr = ''
+    for i in n:
+        r = i.strip()
+        r = i[0].upper() + ''.join(ch for ch in i[1:])
+        rr += r+' '#produce extra space at the end
+    return rr.strip()
+print(title_man(" multiple  spaces  "))
+print(title_man("john's book"))
+def title_man(name):
+    words  = name.split()
+    result = ''
+    for w in words:
+        result+= w[0].upper()+ w[1:] + ' '
+    return result.strip()
+print(title_man('the man of honour'))
