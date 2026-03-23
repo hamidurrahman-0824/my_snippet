603c1b3856e857c497ee9377b1a1d68eb0d04e34
Hamidur Rahman Majed <2022334042@student.sust.edu>
2026-03-23 20:21:35 +0600

    folder created

--- 149-custom-iterator.py
+++ 149-custom-iterator.py
@@ -6,6 +6,8 @@ class Count:
     #     print("in iter block")
     #     print(self)
     #     return self
+    def __iter__(self):
+        return self
     def __next__(self):
         print("in next block")
         if self.current<self.limit:
@@ -14,5 +16,42 @@ class Count:
         else:
             raise StopIteration
 # x = Count(10)
-for x in Count(5):
-    print(x)
+# for x in Count(5):
+#     print(x)
+class EvenNumbers:
+    
+    def __init__(self, limit):
+        self.limit = limit
+        self.num = 0
+        
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.num += 2
+        
+        if self.num <= self.limit:
+            return self.num
+        
+        raise StopIteration
+class Counter:
+    def __init__(self,start):
+        self.start = start
+        
+    def __iter__(self):
+        return self
+    def __next__(self):
+        if self.start<=0:
+            raise StopIteration
+
+        else:
+            self.start-=1
+            return self.start
+
+obj = iter(Counter(5))
+print(next(obj))
+print(next(obj))
+print(next(obj))
+print(next(obj))
+print(next(obj))
+print(next(obj))
