56725d8b6625ac8ac24df66418c040838ceaf603
Hamidur Rahman Majed <2022334042@student.sust.edu>
2026-04-26 17:30:50 +0600

    first 3 segment

--- PANDAS/new.py
+++ PANDAS/new.py
@@ -1,7 +1,31 @@
 import pandas as pd
 df = pd.read_csv("/home/dwgls/my_snippet/synthetic_dataset.csv")
+
+g = df.groupby("Category")["Stock"].value_counts(ascending=True)
+
+
+order = ["In Stock", "Out of Stock"]
+
+g = (
+    df.groupby("Category")["Stock"]
+    .value_counts()
+    .reindex(order, level=1)
+)
+table = df.groupby("Category")["Stock"].value_counts().unstack(fill_value=0)
+g= df.groupby(["Stock","Category"])["Discount"].agg(['max','mean','min','sum'])
+
+result = table.idxmax()
+#oneliner
+df.groupby("Category")["Stock"].value_counts().unstack(fill_value=0).idxmax()
+
+g = df.groupby(["Category"])[["Discount","Price",'Rating']].mean().unstack().idxmax()
+print(df[["Discount",'Rating']].corr())
+
+exit()
 #df = df.dropna(subset=["Price"])
-#df = df.dropna(subset=["Price"])
+df = df.dropna(subset=["Price"])
+x = df.groupby("Category")["Price"].agg(['sum','max','mean','min'])
+print(x)
 """
 h = df.head()
 price = round(df["Price"].mean(),2)
@@ -20,7 +44,7 @@ print(summary)
 
 
 """
-df = df.dropna(subset=["Stock"])
-stock = df.groupby("Stock").size().to_string()
-hrating = df.groupby("Category")["Rating"].sum()
-print(hrating)
+prc = df["Stock"].value_counts(normalize=True)*100
+tt = df.groupby("Stock").count()
+maxminavg = df["Price"].agg(['mean','max','min'])
+print(maxminavg)
