fc6158b2d61975b2579beaecc5c83859059b8c63
Hamidur Rahman Majed <2022334042@student.sust.edu>
2026-05-09 01:04:20 +0600

    General Part ended

--- PANDAS/sana.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-df = pd.read_csv("/home/dwgls/my_snippet/synthetic_dataset.csv")
-grp = df.groupby("Category")
-print(grp["Price"].sum())
-print(df["Category"].value_counts().idxmax())
